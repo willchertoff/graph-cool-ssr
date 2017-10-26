73726bbbc6bc6797a24941e1f3c3d46713fd4eff
Will Chertoff <willchertoff@gmail.com>
2017-10-26 14:42:45 -0700

    Remove cache

--- server.js
+++ server.js
@@ -29,14 +29,6 @@ app.prepare()
   })
 })
 
-/*
- * NB: make sure to modify this to take into account anything that should trigger
- * an immediate page change (e.g a locale stored in req.session)
- */
-function getCacheKey (req) {
-  return `${req.url}`
-}
-
 function renderApp (req, res, pagePath, queryParams) {
   app.renderToHTML(req, res, pagePath, queryParams)
     .then((html) => {
